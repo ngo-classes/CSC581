7ed7d007f5652380df0b02cd2b45d592137c8b66
Linh B Ngo <lngo@wcupa.edu>
2025-03-24 21:40:23 -0400

    Update install.sh

--- docker/base/install.sh
+++ docker/base/install.sh
@@ -9,3 +9,5 @@ log_info() {
 conda install -y jupyter numpy matplotlib pandas nltk scikit-learn ipykernel opencv-python scipy pooch 
 conda install -y tensorflow tensorflow_hub tensorflow_datasets tensorflow-model-optimization pyaudio tensorflow_io 
 conda install -y python_speech_features librosa seaborn
+
+mkdir -p /opt/workplace
