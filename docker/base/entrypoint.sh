967cbb79043f4157334e814b3c8f63c20ceb5732
Linh B Ngo <lngo@wcupa.edu>
2025-03-24 21:41:27 -0400

    Update entrypoint.sh

--- docker/base/entrypoint.sh
+++ docker/base/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-cd /opt/workplace && jupyter lab --port=8888 --NotebookApp.token='' --no-browser --ip=0.0.0.0 --allow-root &
+cd /opt/workplace 
+jupyter lab --port=8888 --NotebookApp.token='' --no-browser --ip=0.0.0.0 --allow-root &
